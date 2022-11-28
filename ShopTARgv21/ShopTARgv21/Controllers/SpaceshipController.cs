48d64a40dc0d92b0f48dcaf238b09f5b3696d243
Ingvar Derešivski <inde999@gmail.com>
2022-11-28 11:34:43 +0200

    SpaceshipListViewAndControllerIndex

--- ShopTARgv21/ShopTARgv21/Controllers/SpaceshipController.cs
+++ ShopTARgv21/ShopTARgv21/Controllers/SpaceshipController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv21.Data;
+using ShopTARgv21.Models.Spaceship;
 
 namespace ShopTARgv21.Controllers
 {
     public class SpaceshipController : Controller
     {
+        private readonly ShopDbContext _context;
+
+        public SpaceshipController
+            (
+                ShopDbContext context
+            )
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Spaceship
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipListViewModel
+                {
+                    Name = x.Name,
+                    ModelType = x.ModelType,
+                    Passengers = x.Passengers,
+                    BuildOfDate = x.BuildOfDate,
+                    LaunchDate = x.LaunchDate
+                });
+
+            return View(result);
         }
     }
 }
